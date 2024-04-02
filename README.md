eef078718fc17d20847c358cae5d05454e1b2ae1
f57709jg <jacob.georgis@student.manchester.ac.uk>
2024-04-02 10:46:40 +0100

    Transfer plugin to github template repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Obsidian PDF to Images
+
+This plugin takes a pdf which is in the vault and converts them all to images, 
+
+**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+
+**Dependencies:**
+https://github.com/yakovmeister/pdf2image/blob/master/docs/gm-installation.md
+
+## Usage
+Click on the icon to the left and select a pdf file from the vault.
---
+++ main.ts
@@ -0,0 +1,93 @@
+import { App, Editor, MarkdownView, Modal, Notice, Menu, Plugin, PluginSettingTab, SuggestModal, Setting, TFile, Vault } from 'obsidian';
+import { fromPath } from "pdf2pic";
+
+// Remember to rename these classes and interfaces!
+
+interface ConvertPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: ConvertPluginSettings = {
+	mySetting: 'default'
+}
+
+export default class ConvertPlugin extends Plugin {
+	settings: ConvertPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		this.addRibbonIcon('file-down', 'Import PDF as image', (event) => {
+			new FileModal(this.app).open();
+		});
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		});
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	onunload() {
+		
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+
+export class FileModal extends SuggestModal<TFile> {
+	getSuggestions(query: string): TFile[] | Promise<TFile[]> {
+		return this.app.vault.getFiles().filter(t => {
+			return t.extension.toLowerCase() == "pdf" && t.name.toLowerCase().includes(query.toLowerCase());
+		})
+	}
+
+	renderSuggestion(value: TFile, el: HTMLElement) {
+		el.createEl("div", {text: value.name});
+		el.createEl("small", {text: value.path});
+	}
+	onChooseSuggestion(item: TFile, evt: MouseEvent | KeyboardEvent) {
+		const baseOptions = {
+			density: 100, 
+			preserveAspectRatio: true,
+			format: "jpeg",
+			saveFilename: item.basename,
+			savePath: "",
+		}
+		const uri = this.app.vault.getResourcePath(item);
+		const split = uri.split('/');
+		var filepath = '';
+		var savepath = '';
+		for(let i = 3; i < split.length - 1; i++) {
+			filepath += split[i] + "/";
+		}
+
+		savepath = decodeURI(filepath);
+		filepath = decodeURI(filepath + item.name);
+
+		baseOptions.savePath = savepath; 
+		const convert = fromPath(filepath, baseOptions).bulk(-1);
+		convert.then((imagesList) => {
+			const currentDoc = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if(currentDoc) {
+				this.app.vault.process(currentDoc.file!, data => {
+					imagesList.forEach((f) => {
+						data += `![[${f.name}|500]]\n`;
+					})
+					return data;
+				})
+				
+			}
+		})
+	}
+
+}
